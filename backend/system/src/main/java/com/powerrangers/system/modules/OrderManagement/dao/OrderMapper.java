f4b296f922d0d35949d31da0de8ddf37db0d40a2
haoyuwang <wanghaoyu1323@gmail.com>
2022-07-27 13:17:43 +0800

    add queryOrder related interfaces

--- backend/system/src/main/java/com/powerrangers/system/modules/OrderManagement/dao/OrderMapper.java
+++ backend/system/src/main/java/com/powerrangers/system/modules/OrderManagement/dao/OrderMapper.java
@@ -1,7 +1,10 @@
 package com.powerrangers.system.modules.OrderManagement.dao;
 
+import com.powerrangers.system.modules.OrderManagement.domain.Order;
 import com.powerrangers.system.modules.OrderManagement.service.dto.OrderDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -9,4 +12,12 @@ public interface OrderMapper {
     void insertOrder(OrderDTO orderDTO);
 
     void refundOrder(OrderDTO orderDTO);
+
+    Order getOrder(OrderDTO orderDTO);
+
+    List<Order> queryOrdersByCustomer(OrderDTO orderDTO);
+
+    List<Order> queryOrdersByHost(OrderDTO orderDTO);
+
+    List<Order> getAllOrders(OrderDTO orderDTO);
 }
