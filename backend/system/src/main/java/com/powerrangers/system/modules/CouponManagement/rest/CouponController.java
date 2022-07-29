ada7cea562f6e8da6e70d6272c1e8a79bdcefe99
haoyuwang <wanghaoyu1323@gmail.com>
2022-07-29 19:20:02 +0800

    fix bugs of event interfaces

--- backend/system/src/main/java/com/powerrangers/system/modules/CouponManagement/rest/CouponController.java
+++ backend/system/src/main/java/com/powerrangers/system/modules/CouponManagement/rest/CouponController.java
@@ -1,9 +1,9 @@
 package com.powerrangers.system.modules.CouponManagement.rest;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CouponController {
 
-
+    @ApiOperation("add coupon")
+    @PostMapping("addCoupon")
+    public ResponseEntity<Object> addCoupon(@RequestHeader("Authorization") String token) {
+        return null;
+    }
 }
--- backend/system/src/main/java/com/powerrangers/system/modules/EventManagement/dao/EventTicketMapper.java
+++ backend/system/src/main/java/com/powerrangers/system/modules/EventManagement/dao/EventTicketMapper.java
@@ -8,6 +8,8 @@ import java.util.List;
 @Mapper
 public interface EventTicketMapper {
 
+    Integer getTicketTypeIdByTicketTypeName(String ticketType);
+
     void insertEventTicketType(TicketDTO ticketDTO);
 
     List<TicketDTO> getTicketType(TicketDTO ticketDTO);
